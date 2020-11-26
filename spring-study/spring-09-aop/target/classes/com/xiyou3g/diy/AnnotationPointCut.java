8341cf9a66ec4057934531ecbbac162e5b574ef7
libiqi <772858741@qq.com>
2020-11-26 12:03:22 +0800

    update my AOP learning

---
+++ spring-study/spring-09-aop/target/classes/com/xiyou3g/diy/AnnotationPointCut.java
@@ -0,0 +1,22 @@
+package com.xiyou3g.diy;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+//方式三：使用注解方式实现AOP
+
+@Aspect//标注这个类是一个切面
+public class AnnotationPointCut {
+
+    @Before("execution(* com.xiyou3g.service.UserServiceImpl.*(..))")
+    public void before()
+    {
+        System.out.println("======方法执行前======");
+    }
+    @After("execution(* com.xiyou3g.service.UserServiceImpl.*(..))")
+    public void after()
+    {
+        System.out.println("======方法执行后======");
+    }
+}
