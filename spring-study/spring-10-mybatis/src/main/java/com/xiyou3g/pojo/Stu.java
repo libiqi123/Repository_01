7305f0a3055c2d791e006f6cc9a66134ce1915c7
libiqi <772858741@qq.com>
2020-11-29 17:46:36 +0800

    update Mybatis review

---
+++ spring-study/spring-10-mybatis/src/main/java/com/xiyou3g/pojo/Stu.java
@@ -0,0 +1,16 @@
+package com.xiyou3g.pojo;
+
+public class Stu {
+    private String name;
+    private int age;
+    private int grade;
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade=" + grade +
+                '}';
+    }
+}
